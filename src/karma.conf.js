b8253c010670870c61c75ded2782c38cee40e595
Yasuyuki Saito <yasuyuki.saito.2012@gmail.com>
2018-05-26 00:39:55 +0900

    テスト結果をjunitフォーマット出力

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -2,41 +2,48 @@
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
 module.exports = function (config) {
-  config.set({
-    basePath: '',
-    frameworks: ['jasmine', '@angular-devkit/build-angular'],
-    plugins: [
-      require('karma-jasmine'),
-      require('karma-chrome-launcher'),
-      require('karma-jasmine-html-reporter'),
-      require('karma-coverage-istanbul-reporter'),
-      require('karma-spec-reporter'),
-      require('@angular-devkit/build-angular/plugins/karma')
-    ],
-    client: {
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
-    },
-    coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, '../coverage'),
-      reports: ['html', 'lcovonly'],
-      fixWebpackSourcePaths: true
-    },
-    reporters: ['spec', 'kjhtml'],
-    port: 9876,
-    colors: true,
-    logLevel: config.LOG_INFO,
-    autoWatch: true,
-    browsers: ['Chrome'],
-    singleRun: false,
-    customLaunchers: {
-        ChromeHeadless:  {
-            base:   'Chrome',
-            flags:  [
-                '--headless',
-                '--disable-gpu',
-                '--remote-debugging-port=9222',
-            ],
+    config.set({
+        basePath: '',
+        frameworks: ['jasmine', '@angular-devkit/build-angular'],
+        plugins: [
+            require('karma-jasmine'),
+            require('karma-chrome-launcher'),
+            require('karma-jasmine-html-reporter'),
+            require('karma-coverage-istanbul-reporter'),
+            require('karma-spec-reporter'),
+            require('karma-junit-reporter'),
+            require('@angular-devkit/build-angular/plugins/karma')
+        ],
+        client: {
+            clearContext: false // leave Jasmine Spec Runner output visible in browser
+        },
+        coverageIstanbulReporter: {
+            dir: require('path').join(__dirname, '../coverage'),
+            reports: ['html', 'lcovonly'],
+            fixWebpackSourcePaths: true
+        },
+        reporters: ['spec', 'kjhtml', 'junit'],
+        port: 9876,
+        colors: true,
+        logLevel: config.LOG_INFO,
+        autoWatch: true,
+        browsers: ['Chrome'],
+        singleRun: false,
+        customLaunchers: {
+            ChromeHeadless:  {
+                base:   'Chrome',
+                flags:  [
+                    '--headless',
+                    '--disable-gpu',
+                    '--remote-debugging-port=9222',
+                ],
+            }
+        },
+        junitReporter: {
+            outputDir: require('path').join(__dirname, '../dist/test'),
+            outputFile: 'test-results.xml',
+            suite: '',
+            useBrowserName: false
         }
-    }
-  });
+    });
 };
