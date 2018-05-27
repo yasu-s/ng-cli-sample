861e75ed8b37648b31efe3954b9b003f118e8572
Yasuyuki Saito <yasuyuki.saito.2012@gmail.com>
2018-05-27 20:26:58 +0900

    Chromeデバッグ設定追加

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+// #region fields
+
+    /** タイトル */
+    title = 'app';
+
+// #endregion
+
+// #region methods
+
+    /**
+     * クリックイベント
+     */
+    onClick(): void {
+        alert('hoge');
+    }
+
+// #endregion
+
 }
