0add4a7c4febf6f0191fa98ec242a7e79453cf94
Yasuyuki Saito <yasuyuki.saito.2012@gmail.com>
2018-06-14 22:31:06 +0900

    HTTP拡張追加

---
+++ src/app/ext/http.client.extension.ts
@@ -0,0 +1,27 @@
+import { HttpClient, HttpErrorResponse, HttpResponse } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
+
+declare module '@angular/common/http/src/client' {
+
+    interface HttpClient {
+        /**
+         * POST拡張：戻り値がHTTPステータスコード
+         * @param url  URL
+         * @param body データ
+         */
+        postEx(url: string, body: any): Observable<number>;
+    }
+}
+
+HttpClient.prototype.postEx = function(url: string, body: any) {
+    const client = this as HttpClient;
+    return client.post(url, body, { observe: 'response' }).pipe(
+        map((res: HttpResponse<any>) => {
+            return res.status;
+        }),
+        catchError((err: HttpErrorResponse) => {
+            return of(err.status);
+        })
+    );
+};
